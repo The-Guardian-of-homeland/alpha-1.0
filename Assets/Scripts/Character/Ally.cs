e0facfa3fa412415a84441daaec668b2a8c822be
XianJiaming <914311195@qq.com>
2020-04-13 12:48:38 +0800

    first

---
+++ Assets/Scripts/Character/Ally.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ally : Character
+{
+    private int spiritual;
+    private int cost;
+    private int level;
+    private int totalHealth;
+    private int totalSpiritual;
+
+    public void work();
+    public void born();
+    public void die();
+    public void getters();
+    public void setters();
+
+}
---
+++ Assets/Scripts/Character/Character.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character
+{
+    private string job;
+    private int health;
+    private Vector2 position;
+    private Vector2 direction;
+    private string target;
+
+    public void work();
+    public void born();
+    public void die();
+    public void getters();
+    public void setters();
+
+
+}
